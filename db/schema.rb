442729b8f4739b1a36eda163b098880795b8be3e
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-30 11:38:24 +0900

    1対多のアソシエーション実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_023015) do
+ActiveRecord::Schema.define(version: 2021_11_30_023530) do
 
   create_table "rental_houses", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2021_11_30_023015) do
     t.integer "min_on_foot"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rental_house_id"
+    t.index ["rental_house_id"], name: "index_stations_on_rental_house_id"
   end
 
+  add_foreign_key "stations", "rental_houses"
 end
