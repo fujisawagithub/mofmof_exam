31ea8fa9ed007a0c8298fba845047dc1226c117e
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-30 15:12:13 +0900

    未記入時のバリデーション設定

--- app/models/rental_house.rb
+++ app/models/rental_house.rb
@@ -1,3 +1,8 @@
 class RentalHouse < ApplicationRecord
+  validates :name, presence: true
+  validates :fee, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
   has_many :stations
+  accepts_nested_attributes_for :stations, allow_destroy: true
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,6 @@
 class Station < ApplicationRecord
+  validates :route_name, presence: true
+  validates :name, presence: true
+  validates :min_on_foot, presence: true
   belongs_to :rental_house
 end
